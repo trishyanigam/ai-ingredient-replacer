d569722c8d6e43df8228569667faaca51657dec4
Deepak Kumar KAshyap <deepakkumarkashyap78557@gmail.com>
2025-08-07 22:35:21 +0530

    updated code to use gemini api key instead of openrouter

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://user:1234@cluster0.msndzsy.mongodb.net/");
+        await mongoose.connect("YOUR_MONGODB_URI");
         console.log("✅ MongoDB Connected");
     } catch (error) {
         console.error("❌ MongoDB Connection Failed:", error.message);
